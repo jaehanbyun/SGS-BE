62db02b414acf82c083688dc5e9bd3e7323b2fd1
kimdonwoo <ehstjstod@gmail.com>
2023-03-30 14:12:34 +0900

    Feat : 스터디 방 관리 서버 Exception Handler 개발 - 발생할 Exception 관련 status. errorCode, description을 정의해둘 ErrorCode enum class 개발 - Exception 발생시 throw 하기위한 ApplicationException 개발 - throw된 모든 Exception을 받아서 처리 해줄 GlobalControllerAdvice 개발
    resolve #18

--- room/src/main/java/pnu/cse/studyhub/room/controller/RoomController.java
+++ room/src/main/java/pnu/cse/studyhub/room/controller/RoomController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 import pnu.cse.studyhub.room.dto.request.RoomCreateRequest;
 import pnu.cse.studyhub.room.dto.response.Response;
 import pnu.cse.studyhub.room.dto.response.RoomCreateResponse;
+import pnu.cse.studyhub.room.exception.ApplicationException;
+import pnu.cse.studyhub.room.exception.ErrorCode;
 import pnu.cse.studyhub.room.service.RoomService;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/room/group")
@@ -22,6 +26,11 @@ public class RoomController {
     public Response<RoomCreateResponse> create(@RequestBody RoomCreateRequest request){
         // TODO : 이후에 JWT로부터 userId등 user 정보 받을 예정
         String userId = "donu";
+
+        if(userId.equals("donu")){
+            // ErrorCode랑 message만 던지면 자동완성
+            throw new ApplicationException(ErrorCode.EXCEPTION_TEST,String.format("exception test : %s user not founded", userId));
+        }
 
         Long roomId = roomService.create(request.isRoomType(), request.getRoomName(),
                 userId, request.getMaxUser(), request.getRoomChannel());
