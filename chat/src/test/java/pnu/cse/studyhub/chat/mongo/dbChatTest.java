2bf733038fcb8e2ab4924274c3bb1c3a80e901c9
jhl8109 <jhl81094@gmail.com>
2023-04-03 15:50:19 +0900

    Test: 채팅 내역 페이지네이션 테스트 구현
    - 채팅 내역 페이지네이션 테스트 구현, dbChatTest

--- chat/src/test/java/pnu/cse/studyhub/chat/mongo/dbChatTest.java
+++ chat/src/test/java/pnu/cse/studyhub/chat/mongo/dbChatTest.java
@@ -1,29 +1,33 @@
 package pnu.cse.studyhub.chat.mongo;
 
+import com.netflix.discovery.converters.Auto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.data.domain.Page;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import pnu.cse.studyhub.chat.dto.request.ChatRequest;
 import pnu.cse.studyhub.chat.repository.ChatRepository;
+import pnu.cse.studyhub.chat.repository.ChatRepositoryImpl;
 import pnu.cse.studyhub.chat.repository.entity.Chat;
 
-import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.List;
 
 @DataMongoTest
 @Slf4j
 public class dbChatTest {
 
     @Autowired
-    private ChatRepository chatRepository;
+    private MongoTemplate mongoTemplate;
 
     private static ChatRequest chatRequest;
     private static Chat savedChat;
 
     @BeforeAll
-    public static void initChat(){
+    public static void initTest(){
+        //chat
         chatRequest = new ChatRequest();
         chatRequest.setRoomId("test");
         chatRequest.setSenderId("test-user1");
@@ -39,6 +43,7 @@ public class dbChatTest {
     @DisplayName("채팅 저장 테스트")
     @Order(1)
     void saveChat() {
+        ChatRepository chatRepository = new ChatRepositoryImpl(mongoTemplate);
         Chat tempChat = chatRequest.toEntity();
         savedChat = chatRepository.save(tempChat);
         log.info("채팅 저장 : " + savedChat);
@@ -48,10 +53,22 @@ public class dbChatTest {
     @DisplayName("Room Id를 통해 채팅 가져오기")
     @Order(2)
     void findChatByRoomId() {
-        ArrayList<Chat> dbChat = chatRepository.findByRoomId(savedChat.getRoomId()).orElse(null);
+        ChatRepository chatRepository = new ChatRepositoryImpl(mongoTemplate);
+        List<Chat> dbChat = chatRepository.findByRoomId(savedChat.getRoomId());
 
         log.info("채팅 리스트 : " + dbChat);
         Assertions.assertEquals(savedChat.get_id(),dbChat.get(0).get_id());
-        chatRepository.deleteAll(dbChat);
+//        chatRepository.deleteAll(dbChat);
+    }
+    @Test
+    @DisplayName("Room Id와 Paging을 통해 채팅 가져오기")
+    @Order(3)
+    void findChatByRoomIdWithPaging(){
+        ChatRepository chatRepository = new ChatRepositoryImpl(mongoTemplate);
+        Page<Chat> dbChat = chatRepository.findByRoomIdWithPagingAndFiltering(savedChat.getRoomId(), 1,1);
+        List<Chat> chatList = dbChat.getContent();
+        log.info("채팅 리스트 : " + dbChat);
+        Assertions.assertEquals(savedChat.get_id(),chatList.get(0).get_id());
+        chatRepository.deleteAll(chatList);
     }
 }
