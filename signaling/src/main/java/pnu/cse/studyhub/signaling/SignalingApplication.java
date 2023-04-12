9f0db423882d159be19fb88efe6b49e592d5da9c
kimdonwoo <ehstjstod@gmail.com>
2023-04-12 22:18:28 +0900

    setting : signaling service

---
+++ signaling/src/main/java/pnu/cse/studyhub/signaling/SignalingApplication.java
@@ -0,0 +1,13 @@
+package pnu.cse.studyhub.signaling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalingApplication.class, args);
+	}
+
+}
---
+++ signaling/src/test/java/pnu/cse/studyhub/signaling/SignalingApplicationTests.java
@@ -0,0 +1,13 @@
+package pnu.cse.studyhub.signaling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
