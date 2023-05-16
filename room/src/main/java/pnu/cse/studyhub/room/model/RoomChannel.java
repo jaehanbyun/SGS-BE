a12575a68633a92c93b2220370f38e4ae24c43d4
jhl8109 <jhl81094@gmail.com>
2023-05-16 11:46:08 +0900

    Revert "Merge remote-tracking branch 'origin/develop/signaling' into develop/signaling"
    This reverts commit 135ec36629618f95cdee84834bf0289ba99b29eb.

--- room/src/main/java/pnu/cse/studyhub/room/model/RoomChannel.java
+++
@@ -1,9 +0,0 @@
-package pnu.cse.studyhub.room.model;
-
-public enum RoomChannel {
-    ALL,
-    ELEMENTARY_SCHOOL,
-    MIDDLE_SCHOOL,
-    HIGH_SCHOOL,
-    UNIVERSITY
-}
--- state/src/main/java/pnu/cse/studyhub/state/repository/RedisRepository.java
+++
@@ -1,9 +0,0 @@
-package pnu.cse.studyhub.state.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pnu.cse.studyhub.state.repository.entity.RealTimeData;
-
-@Repository
-public interface RedisRepository extends CrudRepository<RealTimeData, String> {
-}
