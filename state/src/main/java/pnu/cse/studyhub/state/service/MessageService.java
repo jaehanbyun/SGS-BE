1170652ff96e4b46b5c0cb4e90c6b16778bc38ba
jhl8109 <jhl81094@gmail.com>
2023-05-18 00:03:28 +0900

    Feat : 시그널링 서버에서 전달받은 실시간 정보 저장
    - RealTimeData 구조 디자인
    - Jackson JsonTypeInfo를 통해 서버별 json 반영
    - RedisRepository를 통해 JSON 형태로 Redis에 저장하도록 구현

--- state/src/main/java/pnu/cse/studyhub/state/service/MessageService.java
+++ state/src/main/java/pnu/cse/studyhub/state/service/MessageService.java
@@ -6,7 +6,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import pnu.cse.studyhub.state.dto.request.TCPSocketSessionRequest;
+import pnu.cse.studyhub.state.dto.request.TCPChatRequest;
+import pnu.cse.studyhub.state.dto.request.TCPMessageRequest;
+import pnu.cse.studyhub.state.dto.request.TCPSignalingRequest;
+import pnu.cse.studyhub.state.repository.entity.RealTimeData;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
@@ -19,16 +25,65 @@ public class MessageService {
         log.info("Received message: {}", message);
         ObjectMapper mapper = new ObjectMapper();
         try{
-            TCPSocketSessionRequest response = mapper.readValue(message, TCPSocketSessionRequest.class);
+            TCPMessageRequest response = mapper.readValue(message, TCPMessageRequest.class);
             String responseMessage = String.format("Message \"%s\" is processed", response.toString());
-            log.info("Sending response: {}", responseMessage);
-            if (response.getType() == "CONNECT") {
-                redisService.setValues(response.getUserId(), response.getSession());
-            }
-            else if (response.getType() == "DISCONNECT") {
-                redisService.delValues(response.getUserId());
-            } else {
-                // 에러처리
+            switch (response.getServer()) {
+                case "chat":
+                    TCPChatRequest chatRequest = (TCPChatRequest) response;
+                    log.warn(chatRequest.toString());
+                    if (chatRequest.getType().matches("SUBSCRIBE")) {
+                        RealTimeData realTimeData =  redisService.getData(chatRequest.getUserId());
+                        if (realTimeData == null) {
+                            realTimeData = new RealTimeData();
+                            realTimeData.setUserId(chatRequest.getUserId());
+                            realTimeData.setRoomId(chatRequest.getRoomId());
+                            realTimeData.setSessionId(chatRequest.getSession());
+                            RealTimeData realTimeData1 =  redisService.setData(realTimeData);
+                        } else {
+                            realTimeData.setRoomId(chatRequest.getRoomId());
+                            realTimeData.setSessionId(chatRequest.getSession());
+                            RealTimeData realTimeData2 = redisService.setData(realTimeData);
+                        }
+
+                    } else if (chatRequest.getType().matches("DISCONNECT")) {
+                        RealTimeData realTimeData = redisService.getData(chatRequest.getUserId());
+                        if (realTimeData != null) {
+                            realTimeData.setRoomId(chatRequest.getRoomId());
+                            realTimeData.setSessionId(null);
+                            redisService.setData(realTimeData);
+                        }
+                    } else {
+                        // 에러처리
+                    }
+                    break;
+                case "signaling":
+                    TCPSignalingRequest signalingRequest = (TCPSignalingRequest) response;
+                    log.warn(signalingRequest.toString());
+                    if (signalingRequest.getAction().matches("TIMER_ON")) {
+                        try {
+                            RealTimeData realTimeData =  redisService.getData(signalingRequest.getUserId());
+                            LocalDateTime currentTime = LocalDateTime.now();
+                            realTimeData.setStudyStartTime(currentTime);
+                            RealTimeData savedData = redisService.setData(realTimeData);
+                        }catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else if (signalingRequest.getAction().matches("TIMER_OFF")) {
+                        try {
+                            RealTimeData realTimeData =  redisService.getData(signalingRequest.getUserId());
+                            LocalDateTime currentTime = LocalDateTime.now();
+                            LocalDateTime studyStartTime = realTimeData.getStudyStartTime();
+                            Duration totalStudyTime = Duration.between(studyStartTime, currentTime);
+                            realTimeData.setStudyStartTime(null);
+                            realTimeData.setRecordTime(totalStudyTime);
+                            RealTimeData savedData = redisService.setData(realTimeData);
+                        }catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else {
+                        // 에러처리
+                    }
+                    break;
             }
             return responseMessage;
         } catch (JsonMappingException e) {
