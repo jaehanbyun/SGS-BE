a79c6e250f2743a1a27e60bbaaa91c18bdf05ef8
jhl8109 <jhl81094@gmail.com>
2023-05-30 00:55:38 +0900

    Feat : 상태관리서버 - 스터디방 접속 해제 알림 구현
    - Room서버 TCP 연결(port : 8089)을 위한 TCP Client 설정
    - 시그널링 서버에서 연결 해제 시 room 서버에 접속 해제 알림

--- chat/src/main/java/pnu/cse/studyhub/chat/config/tcp/TCPMessageService.java
+++ chat/src/main/java/pnu/cse/studyhub/chat/config/tcp/TCPMessageService.java
@@ -1,17 +1,16 @@
 package pnu.cse.studyhub.chat.config.tcp;
 
+import io.jsonwebtoken.Jwt;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class TCPMessageService {
     private final TCPClientGateway tcpClientGateway;
-
 
     public void sendMessage(String message) {
         String timestamp = LocalDateTime.now().toString();
