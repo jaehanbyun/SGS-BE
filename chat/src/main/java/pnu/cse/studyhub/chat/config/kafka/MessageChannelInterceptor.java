83ef963e8f7671bbff1b9b6ed77cb7aad57c8e46
jhl8109 <jhl81094@gmail.com>
2023-05-29 23:45:33 +0900

    Feat : Jwt 토큰 userId로 접속 상태 관리
    - MessageChannelInterceptor에 접속 상태 저장 시 jwt 토큰의 userId로 저장.

--- chat/src/main/java/pnu/cse/studyhub/chat/config/kafka/MessageChannelInterceptor.java
+++ chat/src/main/java/pnu/cse/studyhub/chat/config/kafka/MessageChannelInterceptor.java
@@ -2,7 +2,6 @@ package pnu.cse.studyhub.chat.config.kafka;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
@@ -10,19 +9,25 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 import pnu.cse.studyhub.chat.config.tcp.TCPMessageService;
-import pnu.cse.studyhub.chat.dto.request.TCPMessageRequest;
 import pnu.cse.studyhub.chat.dto.request.TCPSocketSessionRequest;
+import pnu.cse.studyhub.chat.service.JwtTokenProvider;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class MessageChannelInterceptor implements ChannelInterceptor {
     private final TCPMessageService tcpMessageService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     // 메세지가 전송되기 전에 Intercept
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+//        jwt 토큰 검증, gateway에서 검증 되었기 때문에 userId를 쓰기만 하면됨.
+//        String authorizationHeader = String.valueOf(accessor.getFirstNativeHeader("Authorization"));
+//        String accessToken = authorizationHeader.replace("Bearer ", "");
+//        String userId = jwtTokenProvider.getUserInfo(accessToken);
+
         if (StompCommand.CONNECT.equals(accessor.getCommand())){
             //jwt 토큰 필터링
             //토큰 데이터를 통해서 header에 userId를 넣어줌
@@ -43,14 +48,18 @@ public class MessageChannelInterceptor implements ChannelInterceptor {
     public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
         String sessionId = accessor.getSessionId();
-        String userId = accessor.getFirstNativeHeader("userId");
+//        jwt 토큰 검증, gateway에서 검증 되었기 때문에 userId를 쓰기만 하면됨.
+        String authorizationHeader = String.valueOf(accessor.getFirstNativeHeader("Authorization"));
+        String accessToken = authorizationHeader.replace("Bearer ", "");
+        String userId = jwtTokenProvider.getUserInfo(accessToken);
+        log.error(authorizationHeader + " : " + accessToken + " : " + userId);
+
         switch (accessor.getCommand()){
             case SUBSCRIBE: // room ID에 들어갈 때(소켓 연결이 아니라 채팅방에 들어갈 때 )
                 TCPSocketSessionRequest subscribeRequest = TCPSocketSessionRequest.builder()
                         .type("SUBSCRIBE")
-                        // "Timer ON TIMER OFF 프론트에서 보내는ㅅ거, USEROUT USErIN 프론트에서 받는거
-//                        .userId(userId)
-                        .userId("test1")
+                        // "Timer ON TIMER OFF 프론트에서 보내는ㅅ거, USER_OUT, USER_IN 프론트에서 받는거
+                        .userId(userId)
                         .server("chat")
                         .roomId(accessor.getDestination().substring(7)) // 슬래쉬 ( '/topic/' ) 삭제
                         .session(sessionId)
@@ -60,9 +69,8 @@ public class MessageChannelInterceptor implements ChannelInterceptor {
             case DISCONNECT: // 채팅방 나갈 때
                 TCPSocketSessionRequest disconnectRequest = TCPSocketSessionRequest.builder()
                         .type("DISCONNECT")
-//                        .userId(userId)
+                        .userId(userId)
                         .server("chat")
-                        .userId("test1")
                         .roomId(accessor.getDestination().substring(7)) // 슬래쉬 ( '/topic/' ) 삭제
                         .session(sessionId)
                         .build();
