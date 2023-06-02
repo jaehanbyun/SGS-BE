bc82a434de0105f8008c20d716e85e2fb9c5f84c
jhl8109 <jhl81094@gmail.com>
2023-06-02 23:16:18 +0900

    Fix 상태관리 Redis 데이터 로직 변경
    - 데이터 저장 시 RealTimeData(JSON), sessionData(STRING)로 분리해서 저장 -> sessionId 으로 데이터 삭제하기 위함.
    - sessionRedisTemplate, realTimeDataRedisTemplate 두개의 빈 활용

--- state/src/main/java/pnu/cse/studyhub/state/repository/RedisRepository.java
+++ state/src/main/java/pnu/cse/studyhub/state/repository/RedisRepository.java
@@ -10,5 +10,6 @@ import java.util.List;
 @Repository
 @EnableRedisRepositories
 public interface RedisRepository extends CrudRepository<RealTimeData, String> {
+    RealTimeData findBySessionId(String sessionId);
 
 }
