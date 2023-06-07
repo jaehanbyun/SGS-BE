0e9746c87b72c2e4afd3f18945c35a4283a065c8
kimdonwoo <ehstjstod@gmail.com>
2023-06-08 01:24:57 +0900

    Feat : 상태관리서버로부터 roomOut TCP 요청 받았을 때, 응답 구현
    - roomOut TCP 요청으로 받은 userId가 해당 room의 방장이었을 때, response에 "DELETE" 넣어서 응답하기
    - 아닌 경우에는 reseponse에 "KEEP" 넣어서 응답하기

--- room/src/main/java/pnu/cse/studyhub/room/service/MessageService.java
+++ room/src/main/java/pnu/cse/studyhub/room/service/MessageService.java
@@ -46,26 +46,19 @@ public class MessageService {
                                 new ApplicationException(ErrorCode.User_NOT_FOUND, String.format("%s not founded", stateRequest.getUserId())));
 
                         if(openUserRoom.isRoomOwner()){ // 방장이면 openRoomEntity 삭제
-                            // TODO : 여기서 방 폭파 TCP 설정
-                            //        responseMessage(userId, roomId)에 추가해주기
                             openRoomRepository.delete(openRoomEntity.get());
-
+                            responseMessage = "DELETE";
 
                         }else{ // 일반 유저면 openRoomEntity 인원 수 감소
                             openRoomEntity.get().minusUser();
-
+                            responseMessage = "KEEP";
                         }
 
-                    } // 스터디 그룹일때는 ..?
-
-                    /*
-                        1. roomId가 존재하는 room(openRoomEntity)인지 확인
-                        2. userRoom으로 해당방에 user 있는지 체크
-                        3. 해당 userId가 해당 roomId의 roomOwner이면 삭제
-                            아니면 방의 인원만 감소
-
-                     */
-
+                    }else{
+                        responseMessage = "KEEP";
+                    }
+                    break;
+                default:
                     break;
             }
             //log.warn("responseMessage : " + responseMessage);
