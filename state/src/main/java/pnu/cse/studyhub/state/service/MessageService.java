dcc06aac96d99e872d6dda8c442a4e2580caa2c8
jhl8109 <jhl81094@gmail.com>
2023-06-12 13:00:42 +0900

    Refactor : 시그널링서버, 유저서버의 일관성을 위한 리팩토링
     - USER_STUDY_TIME -> STUDY_TIME_FROM_TCP로 코드 통일

--- state/src/main/java/pnu/cse/studyhub/state/service/MessageService.java
+++ state/src/main/java/pnu/cse/studyhub/state/service/MessageService.java
@@ -101,7 +101,7 @@ public class MessageService {
                     TCPAuthReceiveRequest authRequest = (TCPAuthReceiveRequest) response;
                     log.warn(authRequest.toString());
                     // 유저 서버에 공부 시간 전달
-                    if (authRequest.getType().matches("USER_STUDY_TIME")) {
+                    if (authRequest.getType().matches("STUDY_TIME_FROM_TCP")) {
                         try {
                             RealTimeData realTimeData =  redisService.findRealTimeData(authRequest.getUserId());
                             if (realTimeData != null) {
