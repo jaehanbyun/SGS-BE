5dcd3abcf8789bebe48885166f75b6ef2cc28374
jhl8109 <jhl81094@gmail.com>
2023-06-20 01:12:49 +0900

    Build : 상태관리서버->유저서버 TCP 전송 설정
    - Application.yml에 host, port, connection pool 추가
    - UserStudyTime -> UserDto 리팩토링
    - Gateway(Interface), Config 파일 추가

---
+++ state/src/main/java/pnu/cse/studyhub/state/config/TCPAuthClientGateway.java
@@ -0,0 +1,10 @@
+package pnu.cse.studyhub.state.config;
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.stereotype.Component;
+
+@Component
+@MessagingGateway(defaultRequestChannel = "AuthOutboundChannel")
+public interface TCPAuthClientGateway {
+    String send(String message);
+}
