4add9a655edbed91fb03b35cbeb0894e1b860216
jhl8109 <jhl81094@gmail.com>
2023-06-20 01:16:54 +0900

    Feat : 시그널링서버 -> 상태관리서버 -> 유저서버 특정 시간 데이터 스케줄링 및 일괄 처리 구현
    - 시그널링 서버->상태관리서버 특정시각(05:00 AM) 스케줄링 데이터 수신 구현
      - 20개씩 나누어 수신 구현
      - 마지막 수신 시 Auth 서버에 Redis 데이터 일괄 전송 구현
    - Redis 데이터 일괄 조회 구현
    추가적으로 Dto 파일 간단한 리팩토링

--- state/src/main/java/pnu/cse/studyhub/state/service/RedisService.java
+++ state/src/main/java/pnu/cse/studyhub/state/service/RedisService.java
@@ -3,12 +3,15 @@ package pnu.cse.studyhub.state.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.DataType;
+import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.stereotype.Service;
 import pnu.cse.studyhub.state.repository.RedisRepository;
 import pnu.cse.studyhub.state.repository.entity.RealTimeData;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -45,4 +48,25 @@ public class RedisService {
         realTimeDataRedisTemplate.delete("realTimeData:" + userId);
         sessionRedisTemplate.delete("sessionIdIndex:" + sessionId);
     }
+
+    public List<RealTimeData> getAllRealTimeData() {
+        //  "realTimeData:*"에 해당하는 모든 키를 가져옴
+        Set<String> keys = realTimeDataRedisTemplate.keys("realTimeData:*");
+        List<RealTimeData> allData = new ArrayList<>();
+
+        if (keys != null) {
+            for(String key : keys) {
+                String userId = key.replace("realTimeData:", "");
+
+                // Fetch the data for each user
+                RealTimeData realTimeData = findRealTimeData(userId);
+                if(realTimeData != null) {
+                    allData.add(realTimeData);
+                }
+            }
+        }
+
+        return allData;
+    }
+
 }
