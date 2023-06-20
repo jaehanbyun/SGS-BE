b501d3270e30cb0dafda9183f0f504104083936a
jhl8109 <jhl81094@gmail.com>
2023-06-20 10:18:39 +0900

    Test : 배치 스케줄링 TCP, Redis 테스트
    - Mock 을 활용한 유닛 테스트 구현
      - Redis 모든 데이터 일괄 조회 테스트
      - 조회에 따른 TCP 전송 테스트

--- state/src/test/java/pnu/cse/studyhub/state/RedisServiceTest.java
+++ state/src/test/java/pnu/cse/studyhub/state/RedisServiceTest.java
@@ -2,23 +2,23 @@ package pnu.cse.studyhub.state;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import pnu.cse.studyhub.state.repository.entity.RealTimeData;
 import pnu.cse.studyhub.state.service.RedisService;
 
-import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
-@SpringBootTest
-@Slf4j
+import static org.mockito.Mockito.when;
+
+
 class RedisServiceTest {
 
-    @Autowired
+    @Mock
     private RedisService redisService;
-
+    public RealTimeData realTimeData = new RealTimeData();
+    public RealTimeData realTimeData2 = new RealTimeData();
     private static final String TEST_ID = "testId1";
     private static final Long TEST_ROOM = 1L;
     private static final String TEST_SESSION = "testSession";
@@ -30,21 +30,18 @@ class RedisServiceTest {
 
     @BeforeEach
     void setUp() {
-        RealTimeData realTimeData = new RealTimeData();
+        MockitoAnnotations.openMocks(this);  // Initialize the mock objects
+
         realTimeData.setUserId(TEST_ID);
         realTimeData.setRoomId(TEST_ROOM);
         realTimeData.setSessionId(TEST_SESSION);
         realTimeData.setStudyTime(TEST_STUDYTIME);
 
-        RealTimeData realTimeData2 = new RealTimeData();
         realTimeData2.setUserId(TEST_ID2);
         realTimeData2.setRoomId(TEST_ROOM2);
         realTimeData2.setSessionId(TEST_SESSION2);
         realTimeData2.setStudyTime(TEST_STUDYTIME2);
 
-        // Clear any existing data in Redis before each test
-        redisService.saveRealTimeData(realTimeData);
-        redisService.saveRealTimeData(realTimeData2);
     }
 //    @AfterEach
 //    void tearDown() {
@@ -54,17 +51,29 @@ class RedisServiceTest {
 //    }
 
     @Test
+    @DisplayName("데이터 저장 테스트")
     void getDataTest() {
-        // Call the setValues method
+        when(redisService.findRealTimeData(TEST_ID)).thenReturn(realTimeData);
+
         RealTimeData savedData = redisService.findRealTimeData(TEST_ID);
         Assertions.assertEquals(TEST_SESSION, savedData.getSessionId());
         System.out.println(savedData);
+
     }
     @Test
-    void getDataListTest(){
-        RealTimeData savedData = redisService.findRealTimeData(TEST_ID);
-        System.out.println(savedData);
-        Assertions.assertEquals(TEST_SESSION, savedData.getSessionId());
+    @DisplayName("모든 데이터 일괄 조회 테스트")
+    void getAllDataListTest(){
+        when(redisService.getAllRealTimeData()).thenReturn(Arrays.asList(realTimeData, realTimeData2));
+
+
+        List<RealTimeData> allData = redisService.getAllRealTimeData();
+        List<RealTimeData> testCases = List.of(
+                realTimeData,
+                realTimeData2
+        );
+        Assertions.assertEquals(2, allData.size());
+        Assertions.assertEquals(allData, testCases);
+        System.out.println(testCases + " VS " + allData);
 
     }
 
