31e7111cce493a2498944a7de9c37b03a5f3df95
jhl8109 <jhl81094@gmail.com>
2023-06-22 01:27:47 +0900

    Test : Mock을 활용한 redisService 테스트 코드 작성
    - realTimeDataRedisTemplate, sessionRedisTemplate -> RedisService
    - 모든 데이터 일괄 조회 테스트
    - 세션을 통한 사용자 ID 조회 테스트
    - 사용자 ID를 통한 데이터 조회 테스트
    - 모든 데이터 일괄 삭제 테스트
    - 데이터 저장 테스트
    - 데이터 삭제 테스트

--- state/src/test/java/pnu/cse/studyhub/state/RedisServiceTest.java
+++ state/src/test/java/pnu/cse/studyhub/state/RedisServiceTest.java
@@ -2,20 +2,33 @@ package pnu.cse.studyhub.state;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import pnu.cse.studyhub.state.repository.entity.RealTimeData;
 import pnu.cse.studyhub.state.service.RedisService;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-
+@ExtendWith(MockitoExtension.class)
 class RedisServiceTest {
 
     @Mock
+    private RedisTemplate<String, RealTimeData> realTimeDataRedisTemplate;
+    @Mock
+    private RedisTemplate<String, String> sessionRedisTemplate;
+    @InjectMocks
     private RedisService redisService;
     public RealTimeData realTimeData = new RealTimeData();
     public RealTimeData realTimeData2 = new RealTimeData();
@@ -28,10 +41,12 @@ class RedisServiceTest {
     private static final String TEST_SESSION2 = "testSession2";
     private static final String TEST_STUDYTIME2 = "100102";
 
+
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);  // Initialize the mock objects
-
+        redisService = new RedisService(realTimeDataRedisTemplate, sessionRedisTemplate);
         realTimeData.setUserId(TEST_ID);
         realTimeData.setRoomId(TEST_ROOM);
         realTimeData.setSessionId(TEST_SESSION);
@@ -41,40 +56,101 @@ class RedisServiceTest {
         realTimeData2.setRoomId(TEST_ROOM2);
         realTimeData2.setSessionId(TEST_SESSION2);
         realTimeData2.setStudyTime(TEST_STUDYTIME2);
-
     }
-//    @AfterEach
-//    void tearDown() {
-//        // Clear any existing data in Redis after each test
-//        redisService.delData(TEST_ID);
-//        redisService.delData(TEST_ID2);
-//    }
+
+
 
     @Test
     @DisplayName("데이터 저장 테스트")
     void getDataTest() {
-        when(redisService.findRealTimeData(TEST_ID)).thenReturn(realTimeData);
+        HashOperations<String, Object, Object> hashOperations = Mockito.mock(HashOperations.class);
+        when(realTimeDataRedisTemplate.opsForHash()).thenReturn(hashOperations);
+        when(hashOperations.get("realTimeData:" + TEST_ID, "data")).thenReturn(realTimeData);
 
+        // Perform the test
         RealTimeData savedData = redisService.findRealTimeData(TEST_ID);
+        System.out.println(TEST_SESSION +  savedData.getSessionId());
         Assertions.assertEquals(TEST_SESSION, savedData.getSessionId());
-        System.out.println(savedData);
 
+        // Verify the interactions
+        verify(realTimeDataRedisTemplate.opsForHash(), times(1)).get("realTimeData:" + TEST_ID, "data");
+    }
+
+    @Test
+    @DisplayName("데이터 삭제 테스트")
+    void saveAndDeleteRealTimeDataAndSessionTest() {
+        // Perform deleting operation
+        redisService.deleteRealTimeDataAndSession(TEST_ID, TEST_SESSION);
+        verify(realTimeDataRedisTemplate).delete("realTimeData:" + TEST_ID);
+        verify(sessionRedisTemplate).delete("sessionIdIndex:" + TEST_SESSION);
+    }
+
+    @Test
+    @DisplayName("세션을 통한 사용자 ID 조회 테스트")
+    void findUserIdBySessionIdTest() {
+        ValueOperations<String,String> valueOperations = Mockito.mock(ValueOperations.class);
+        when(sessionRedisTemplate.opsForValue()).thenReturn(valueOperations);
+        when(sessionRedisTemplate.opsForValue().get("sessionIdIndex:" + TEST_SESSION)).thenReturn(TEST_ID);
+
+        String userId = redisService.findUserIdBySessionId(TEST_SESSION);
+        Assertions.assertEquals(TEST_ID, userId);
+
+        verify(sessionRedisTemplate.opsForValue()).get("sessionIdIndex:" + TEST_SESSION);
+    }
+
+    @Test
+    @DisplayName("사용자 ID를 통한 데이터 조회 테스트")
+    void findRealTimeDataByUserIdTest() {
+        HashOperations<String, Object, Object> hashOperations = Mockito.mock(HashOperations.class);
+        when(realTimeDataRedisTemplate.opsForHash()).thenReturn(hashOperations);
+        when(hashOperations.get("realTimeData:" + TEST_ID, "data")).thenReturn(realTimeData);
+
+        RealTimeData rtdata = redisService.findRealTimeData(TEST_ID);
+        Assertions.assertEquals(realTimeData, rtdata);
+
+        verify(realTimeDataRedisTemplate.opsForHash()).get("realTimeData:" + TEST_ID, "data");
     }
     @Test
     @DisplayName("모든 데이터 일괄 조회 테스트")
     void getAllDataListTest(){
-        when(redisService.getAllRealTimeData()).thenReturn(Arrays.asList(realTimeData, realTimeData2));
+        HashOperations<String, Object, Object> hashOperations = Mockito.mock(HashOperations.class);
+        when(realTimeDataRedisTemplate.opsForHash()).thenReturn(hashOperations);
 
+        when(realTimeDataRedisTemplate.keys("realTimeData:*")).thenReturn(Set.of("realTimeData:" + TEST_ID, "realTimeData:" + TEST_ID2));
+        when(realTimeDataRedisTemplate.opsForHash().get("realTimeData:" + TEST_ID, "data")).thenReturn(realTimeData);
+        when(realTimeDataRedisTemplate.opsForHash().get("realTimeData:" + TEST_ID2, "data")).thenReturn(realTimeData2);
 
         List<RealTimeData> allData = redisService.getAllRealTimeData();
-        List<RealTimeData> testCases = List.of(
-                realTimeData,
-                realTimeData2
-        );
-        Assertions.assertEquals(2, allData.size());
-        Assertions.assertEquals(allData, testCases);
-        System.out.println(testCases + " VS " + allData);
+        System.out.println(allData);
 
+        Assertions.assertEquals(2, allData.size());
+        Assertions.assertTrue(allData.containsAll(Arrays.asList(realTimeData, realTimeData2)));
+    }
+
+    @Test
+    @DisplayName("모든 데이터 일괄 삭제 테스트")
+    void deleteAllDataTest(){
+        HashOperations<String, Object, Object> hashOperations = Mockito.mock(HashOperations.class);
+        when(realTimeDataRedisTemplate.opsForHash()).thenReturn(hashOperations);
+
+
+        // Mock the keys
+        Set<String> keys = new HashSet<>(Arrays.asList("realTimeData:" + TEST_ID, "realTimeData:" + TEST_ID2));
+
+        // Mock the session keys
+        Set<String> sessionKeys = new HashSet<>(Arrays.asList("sessionIdIndex:" + TEST_SESSION, "sessionIdIndex:" + TEST_SESSION2));
+
+        // Mock the findRealTimeData() method to return a RealTimeData object with a specific sessionId
+        when(realTimeDataRedisTemplate.keys("realTimeData:*")).thenReturn(keys);
+        when(realTimeDataRedisTemplate.opsForHash().get("realTimeData:" + TEST_ID, "data")).thenReturn(realTimeData);
+        when(realTimeDataRedisTemplate.opsForHash().get("realTimeData:" + TEST_ID2, "data")).thenReturn(realTimeData2);
+
+        // Call the deleteAllData() method
+        redisService.deleteAllData();
+
+        // Verify that the delete() method was called with the correct sets of keys
+        verify(realTimeDataRedisTemplate).delete(keys);
+        verify(sessionRedisTemplate).delete(sessionKeys);
     }
 
 }
