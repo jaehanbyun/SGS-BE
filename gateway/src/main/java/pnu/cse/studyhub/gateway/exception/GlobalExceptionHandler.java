27abe0d163dea5c518e83b2711d6a7e9c61c12dd
wwogusqkr0515@naver.com <wwogusqkr0515@naver.com>
2023-07-10 19:21:13 +0900

    Feat: access 토큰 기간 만료시 리다이렉트 기능 구현
    - access 토큰 만료시 refresh 토큰으로 access 토큰 재발급 리다이렉트 기능 추가
    auth 서버 post 메소드는 필터 제외

--- gateway/src/main/java/pnu/cse/studyhub/gateway/exception/GlobalExceptionHandler.java
+++ gateway/src/main/java/pnu/cse/studyhub/gateway/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package pnu.cse.studyhub.gateway.exception;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureException;
@@ -10,23 +11,39 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.UriComponentsBuilder;
 import pnu.cse.studyhub.gateway.dto.ErrorResponseDto;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Component
 @Order(-1) // 내부 bean 보다 우선 순위를 높여 해당 빈이 동작하게 설정
 @Slf4j
 public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
+//    @Value("${EC2_IP}")
+//    String authUrl;
+    
+    private final WebClient webClient;
+
+    public GlobalExceptionHandler(WebClient.Builder webClientBuilder) {
+        webClient = webClientBuilder.build();
+    }
 
     @Override
     public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
-        log.error("In Exception Handler");
+        log.error("In Exception Handler = {}", ex.toString());
 
         ErrorCode errorCode;
         ErrorResponseDto errorResponseDto;
@@ -34,10 +51,57 @@ public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
                 || ex.getClass() == UnsupportedJwtException.class ) {
             errorCode = ErrorCode.INVALID_TOKEN;
             errorResponseDto = errorCode.toErrorResponseDto("유효하지 않은 토큰");
-        } else if (ex.getClass() == ExpiredJwtException.class){
-            errorCode = ErrorCode.EXPIRED_TOKEN;
-            errorResponseDto = errorCode.toErrorResponseDto("만료된 토큰");
-        } else if (ex.getClass() == JwtTokenMissingException.class) {
+        }
+        else if (ex.getClass() == ExpiredJwtException.class){
+            String refreshToken = exchange.getRequest().getHeaders().getValuesAsList("refreshToken").get(0);
+            String tmp = refreshToken.replace("refreshToken=", "");
+            refreshToken = "";
+            for (int i=0;i<tmp.length();i++) {
+                if (tmp.charAt(i) == ';') break;
+                refreshToken += tmp.charAt(i);
+            }
+            log.error("check refresh token "+refreshToken);
+            URI uri = UriComponentsBuilder
+                    .fromUriString("http://localhost:8080")
+                    .path("/auth/refreshToken")
+                    .build()
+                    .toUri();
+            exchange.getRequest()
+                    .mutate()
+                    .uri(uri)
+                    .header("refresh_token", refreshToken)
+                    .build();
+
+            ServerHttpResponse response = exchange.getResponse();
+
+            String finalRefreshToken = refreshToken;
+            return webClient
+                    .method(HttpMethod.POST)
+                    .uri(uri)
+                    .headers(headers -> headers.add("refresh_token", finalRefreshToken))
+                    .exchange()
+                    .flatMap(clientResponse -> response.writeWith(clientResponse.bodyToMono(DataBuffer.class)));
+
+
+//            HttpClient client = HttpClientBuilder.create().build(); // HttpClient 생성
+//            HttpPost getRequest = new HttpGet(requestURL); //GET 메소드 URL 생성
+//            getRequest.addHeader("x-api-key", RestTestCommon.API_KEY); //KEY 입력
+//
+//            HttpResponse response = client.execute(getRequest);
+//
+//            //Response 출력
+//            if (response.getStatusLine().getStatusCode() == 200) {
+//                ResponseHandler<String> handler = new BasicResponseHandler();
+//                String body = handler.handleResponse(response);
+//                System.out.println(body);
+//            } else {
+//                System.out.println("response is error : " + response.getStatusLine().getStatusCode());
+//            }
+
+//            errorCode = ErrorCode.EXPIRED_TOKEN;
+//            errorResponseDto = errorCode.toErrorResponseDto("만료된 토큰");
+        }
+        else if (ex.getClass() == JwtTokenMissingException.class) {
             errorCode = ErrorCode.MISSING_TOKEN;
             errorResponseDto = errorCode.toErrorResponseDto("토큰이 전달되지 않음");
         } else {
@@ -46,6 +110,7 @@ public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
         }
         ObjectMapper mapper = new ObjectMapper();
         String result = null;
+
         try {
             result = mapper.writeValueAsString(errorResponseDto);
         } catch (JsonProcessingException e) {
--- gateway/src/main/java/pnu/cse/studyhub/gateway/security/AuthorizationHeaderFilter.java
+++ gateway/src/main/java/pnu/cse/studyhub/gateway/security/AuthorizationHeaderFilter.java
@@ -8,18 +8,27 @@ import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFac
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
 import pnu.cse.studyhub.gateway.exception.JwtTokenMissingException;
+
+import java.util.Base64;
+import java.util.Date;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.queryParam;
 
 @Component
 @Slf4j
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
     Environment env;
+    String secretKey;
 
     @Autowired
     public AuthorizationHeaderFilter(Environment env) {
         super(Config.class);
         this.env = env;
+        this.secretKey = env.getProperty("token.secret_key");
     }
     public static class Config {
 
@@ -38,23 +47,98 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
 
             log.error("jwt:"+jwt);
 
-            validateJwtToken(jwt);
+//            if(!isValidToken(jwt)) {
+//                log.error("invalid token");
+//            }
+            String id = isValidToken(jwt);
+//            request.mutate().parameter()
+            ServerHttpRequest modifiedRequest = exchange.getRequest()
+                    .mutate()
+                    .header("id",id)
+                    .build();
+
+
+            ServerWebExchange modifiedExchange = exchange.mutate()
+                    .request(modifiedRequest)
+                    .build();
+
             return chain.filter(exchange);
         };
     }
 
-    public void validateJwtToken(String token) {
-        try {
-            Claims claims = Jwts.parser()
-                    .setSigningKey(env.getProperty("token.secret_key"))
-                    .parseClaimsJws(token)
-                    .getBody();
-            log.error(claims.toString());
-        } catch (SignatureException | MalformedJwtException |
-                 UnsupportedJwtException | IllegalArgumentException | ExpiredJwtException jwtException) {
-            jwtException.printStackTrace();
-            throw jwtException;
-        }
+//    public void validateJwtToken(String token) {
+//        try {
+//            String secretKey = env.getProperty("token.secret_key");
+//            secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+//            log.error("check point for check decode  "+ secretKey);
+//            Claims claims1 = Jwts.parser()
+//                    .setSigningKey(env.getProperty("token.secret_key"))
+//                    .parseClaimsJws(token)
+//                    .getBody();
+//            log.error("check for claim method 1  : "+ claims1.toString());
+//            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+//            log.error("claim : "+claims.toString());
+//        } catch (SignatureException | MalformedJwtException |
+//                 UnsupportedJwtException | IllegalArgumentException | ExpiredJwtException jwtException) {
+//            log.error("error text : "+jwtException.toString());
+//            jwtException.printStackTrace();
+//            throw jwtException;
+//        }
+//    }
+
+    /**
+     * 토큰 유효여부 확인
+     */
+    public String isValidToken(String token) {
+        log.info("isValidToken token = {}", token);
+        if(isTokenExpired(token)) { log.error("check"); }
+        String id = checkValidToken(token);
+
+//        return (token.equals(checkToken) && !isTokenExpired(token));
+        return id;
+    }
+
+    /**
+     * 토큰의 Claim 디코딩
+     */
+    private Claims getAllClaims(String token) {
+        log.info("getAllClaims token = {}", token);
+        log.info("check sigin key= {}", secretKey);
+//        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /**
+     * Claim 에서 username 가져오기
+     */
+    public String checkValidToken(String token) {
+        Claims claims = getAllClaims(token);
+        log.error("Check claims ={}",claims.toString());
+        String id = (String) claims.get("id");
+        return id;
+//        return Jwts.builder()
+//                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+//                .setClaims(claims)
+//                .signWith(SignatureAlgorithm.HS256,secretKey)
+//                .compact();
+    }
+
+    /**
+     * 토큰 만료기한 가져오기
+     */
+    public Date getExpirationDate(String token) {
+        Claims claims = getAllClaims(token);
+        return claims.getExpiration();
+    }
+
+    /**
+     * 토큰이 만료되었는지
+     */
+    private boolean isTokenExpired(String token) {
+        return getExpirationDate(token).before(new Date());
     }
 
 }
