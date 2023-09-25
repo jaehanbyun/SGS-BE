ff55e9829ddd7e498160eeb40023f27e7973585b
wogusqkr0515 <wwogusqkr0515@naver.com>
2023-09-25 17:46:36 +0900

    Fix: 스케줄링 공부시간 저장 null 데이터 예외 처리
    - 스케줄링 공부시간 저장 Null 데이터 예외 처리

--- auth/src/main/java/pnu/cse/studyhub/auth/service/MessageService.java
+++ auth/src/main/java/pnu/cse/studyhub/auth/service/MessageService.java
@@ -52,9 +52,11 @@ public class MessageService {
                 log.info("time test month ={} day = {}", formatedDate.substring(0, 7), formatedDate.substring(8, 10));
                 String month = formatedDate.substring(0, 7);
                 String day = formatedDate.substring(8, 10);
-                String studyTime = jo.getAsString("study_time");
-                if(studyTime.equals(null) || studyTime == null) {
+                String studyTime = "";
+                if(jo.get("study_time") == null) {
                     studyTime = "00:00:00";
+                } else {
+                    studyTime = jo.getAsString("study_time");
                 }
                 UserInfoDto userInfoDto = new UserInfoDto(jo.getAsString("user_id"), month, day, studyTime);
                 User user = User.createInfo(userInfoDto);
