e140dc64a10911290e909469f5fafc990ee47c86
jhl8109 <jhl81094@gmail.com>
2023-11-03 12:12:53 +0900

    Feat : 파티션 분할에 따른 Producer, Consumer 로직 변경
    - 컨슈머 그룹을 3개로 구성
    - roomId에 따라 파티션 분배

--- chat/src/main/java/pnu/cse/studyhub/chat/service/KafkaProducer.java
+++ chat/src/main/java/pnu/cse/studyhub/chat/service/KafkaProducer.java
@@ -12,23 +12,22 @@ import pnu.cse.studyhub.chat.repository.entity.Chat;
 import java.io.IOException;
 
 @Service
-@RequiredArgsConstructor // same as autowired
+@RequiredArgsConstructor
 @Slf4j
 public class KafkaProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void send(String topic, Chat chat) {
-        log.info("producer_topic : " + topic);
+        log.info("producer_topic : " + topic + ", topic : " + topic+"-"+(chat.getRoomId()%3+1));
         log.info("producer_content : " + chat.getContent());
         String type = chat.getMessageType();
         ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
         try {
             String content = objectMapper.writeValueAsString(chat);
-            kafkaTemplate.send(topic, content);
+//            kafkaTemplate.send(topic,  content);
+            kafkaTemplate.send(topic+"-"+(chat.getRoomId()%3+1), Integer.valueOf((int)(chat.getRoomId()%3)),chat.get_id(), content);
         } catch (JsonProcessingException e) { //json 파싱 실패
             e.printStackTrace();
-        } catch (IOException e) { // 파일 변환 실패
-            throw new RuntimeException(e);
         }
     }
 }
