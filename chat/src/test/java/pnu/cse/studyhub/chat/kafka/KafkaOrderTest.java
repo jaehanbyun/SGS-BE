e8fde7b2ad0cef147aee51e7d58933893f063aee
jhl8109 <jhl81094@gmail.com>
2023-11-03 12:13:23 +0900

    Test : 카프카 순서 보장 여부 테스트 구현

---
+++ chat/src/test/java/pnu/cse/studyhub/chat/kafka/KafkaOrderTest.java
@@ -0,0 +1,39 @@
+package pnu.cse.studyhub.chat.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import pnu.cse.studyhub.chat.repository.entity.Chat;
+import pnu.cse.studyhub.chat.service.KafkaConsumer;
+import pnu.cse.studyhub.chat.service.KafkaProducer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@EmbeddedKafka(partitions = 3)
+public class KafkaOrderTest {
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    @Autowired
+    private KafkaConsumer kafkaConsumer;
+
+    @Test
+    public void testMessageOrder() throws InterruptedException {
+        String topic = "studyhub";
+
+        for(int i=0; i<10; i++) {
+            kafkaProducer.send(topic,new Chat(
+                    String.valueOf(i),
+                    (long) i%3,
+                    "test1",
+                    "TEXT",
+                    "test message",
+                    LocalDateTime.now()));
+        }
+        Thread.sleep(5000); // consume하기 위한 대기시간
+    }
+}
