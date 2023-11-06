c1a654aa0d4628bf7c6a4c916f6307971f2927b2
jhl8109 <jhl81094@gmail.com>
2023-11-06 23:08:18 +0900

    Test : Byte 배열 -> 문자열 변환 테스트 구현

---
+++ chat/src/test/java/pnu/cse/studyhub/chat/ByteArrayToStringConverterTest.java
@@ -0,0 +1,27 @@
+package pnu.cse.studyhub.chat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pnu.cse.studyhub.chat.util.ByteArrayToStringConverter;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ByteArrayToStringConverterTest {
+
+    @Test
+    public void testConvert() {
+        ByteArrayToStringConverter converter = new ByteArrayToStringConverter();
+
+        // 테스트할 16진수 문자열
+        String hexValues = "7B 22 75 73 65 72 5F 69 64 22 3A 22 6A 68 6C 38 31 30 39 22 2C 22 73 74 75 64 79 5F 74 69 6D 65 22 3A 22 30 30 3A 30 30 3A 30 37 22 7D";
+
+        // 16진수 문자열을 문자열로 변환
+        String result = converter.convert(hexValues);
+
+        // 변환 결과와 기대값 비교
+        String expected = "{\"user_id\":\"jhl8109\",\"study_time\":\"00:00:07\"}";
+        assertEquals(expected, result);
+    }
+}
